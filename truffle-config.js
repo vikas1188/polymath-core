557a140a9c715cdf55cdcfb2b6270a6594b531b0
Mudit Gupta <guptamudit@ymail.com>
2019-06-03 12:47:41 +0530

    removed extra dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,5 @@
 require('babel-register');
 require('babel-polyfill');
-const fs = require('fs');
-const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
-
-const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 
 module.exports = {
   networks: {
@@ -19,35 +15,6 @@ module.exports = {
       network_id: '1', // Match any network id
       gas: 7900000,
       gasPrice: 10000000000
-    },
-    ropsten: {
-      // provider: new HDWalletProvider(privKey, "http://localhost:8545"),
-      host: 'localhost',
-      port: 8545,
-      network_id: '3', // Match any network id
-      gas: 4500000,
-      gasPrice: 150000000000
-    },
-    rinkeby: {
-      // provider: new HDWalletProvider(privKey, "http://localhost:8545"),
-      host: 'localhost',
-      port: 8545,
-      network_id: '4', // Match any network id
-      gas: 7500000,
-      gasPrice: 10000000000
-    },
-    kovan: {
-      provider: () => {
-        const key = fs.readFileSync('./privKey').toString();
-        let wallet = new HDWalletProvider(key, "https://kovan.infura.io/")
-        var nonceTracker = new NonceTrackerSubprovider()
-        wallet.engine._providers.unshift(nonceTracker)
-        nonceTracker.setEngine(wallet.engine)
-        return wallet
-      },
-      network_id: '42', // Match any network id
-      gas: 7900000,
-      gasPrice: 5000000000
     },
     coverage: {
       host: "localhost",
